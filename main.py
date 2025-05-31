c0170984f3fe7e8391433431fdeb52468f15291e
Thiago Bluhm <thiagobluhm@gmail.com>
2025-05-31 09:48:08 -0300

    apenas arquivos logger split CSS refeito 2 PALETA IQJ

--- main.py
+++ main.py
@@ -3,24 +3,21 @@ import streamlit_authenticator as stauth
 from yaml import safe_load
 from stream_logs_alerta import logs_alerta
 from dotenv import load_dotenv
-
 import os
-
 
 # Precisa vir antes de qualquer comando Streamlit
 st.set_page_config(page_title="Xtracta App", layout="wide")
+
 def load_css(css_file):
     with open(css_file) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-
-# Configuração de usuários e senhas (hash SHA512 gerado antes)
-# Carrega variáveis do .env
+# Carrega variáveis de ambiente
 load_dotenv()
-
 password_hash = os.environ.get("THIAGO_PASSWORD_HASH")
 password_hash2 = os.environ.get("VISITANTE_PASSWORD_HASH")
-# Configurações de autenticação (sem 'preauthorized')
+
+# Configurações de autenticação
 config = {
     'credentials': {
         'usernames': {
@@ -28,7 +25,7 @@ config = {
                 'email': 'thiagobluhm@gmail.com',
                 'name': 'Thiago Bluhm',
                 'password': password_hash
-            }, 
+            },
             'visitante': {
                 'email': 'visitante@portfoliotech.com',
                 'name': 'Visitante Xtracta',
@@ -43,6 +40,7 @@ config = {
     }
 }
 
+# Inicializa autenticação
 authenticator = stauth.Authenticate(
     config['credentials'],
     config['cookie']['name'],
@@ -50,32 +48,35 @@ authenticator = stauth.Authenticate(
     config['cookie']['expiry_days'],
 )
 
-
+# Tela de login
 name, auth_status, username = authenticator.login()
 
-
+# Tratamento do status de login
 if auth_status is False:
     st.error("Usuário ou senha incorretos.")
+
 elif auth_status is None:
     load_css(".streamlit/login.css")
     st.warning("Por favor, insira suas credenciais.")
-    
+
 elif auth_status:
+    # Logo do IQJ na sidebar
     st.sidebar.markdown(
         """
-        <div style="text-align: center; margin-bottom: 1rem;">
+        <div style="text-align: center; margin-top: 1rem; margin-bottom: 1rem;">
             <img src="https://www.institutoqueirozjereissati.org.br/wp-content/uploads/2024/07/IQJ.png" 
-                width="150" style="border-radius: 10px;">
+                 width="150" style="border-radius: 10px;">
         </div>
         """,
         unsafe_allow_html=True
     )
+
+    # Boas-vindas e logout
     st.sidebar.success(f"Bem-vindo, {name} 👋")
     authenticator.logout("Logout", "sidebar")
-    load_css(".streamlit/dashboard.css")
-    # Aqui vai seu app    
-    logs_alerta()
 
-# import streamlit_authenticator as stauth
-# hashes = stauth.Hasher(['**********']).generate()
-# print(hashes[0])
+    # Carrega visual do dashboard
+    load_css(".streamlit/dashboard.css")
+
+    # Chamada do app principal
+    logs_alerta()
