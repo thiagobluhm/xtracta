7546cfb7257f8e1b4ffe6749fb187b316643f97f
Thiago Bluhm <thiagobluhm@gmail.com>
2025-05-30 22:05:07 -0300

    Login feito requirements SENHA AUTH env hash

--- main.py
+++ main.py
@@ -1,33 +1,52 @@
-# import streamlit as st
-# import streamlit_authenticator as stauth
-# from yaml import safe_load
-# from stream_logs_alerta import logs_alerta
-
-# # Configuração de usuários e senhas (hash SHA512 gerado antes)
-# with open('./config.yaml') as file:
-#     config = safe_load(file)
-
-# authenticator = stauth.Authenticate(
-#     config['credentials'],
-#     config['cookie']['name'],
-#     config['cookie']['key'],
-#     config['cookie']['expiry_days'],
-#     config['preauthorized']
-# )
-
-# name, auth_status, username = authenticator.login('Login', 'main')
-
-# if auth_status is False:
-#     st.error("Usuário ou senha incorretos.")
-# elif auth_status is None:
-#     st.warning("Por favor, insira suas credenciais.")
-# elif auth_status:
-#     st.sidebar.success(f"Bem-vindo, {name} 👋")
-#     authenticator.logout("Logout", "sidebar")
-
-#     # Aqui vai seu app    
-#     logs_alerta()
-
+import streamlit as st
 import streamlit_authenticator as stauth
-hashes = stauth.Hasher(['minha_senha_segura']).generate()
-print(hashes[0])
+from yaml import safe_load
+from stream_logs_alerta import logs_alerta
+from dotenv import load_dotenv
+
+# Configuração de usuários e senhas (hash SHA512 gerado antes)
+# Carrega variáveis do .env
+load_dotenv()
+
+password_hash = os.environ.get("THIAGO_PASSWORD_HASH")
+config = {
+    'credentials': {
+        'usernames': {
+            'thiago': {
+                'email': 'thiagobluhm@gmail.com',
+                'name': 'Thiago Bluhm',
+                'password': password_hash
+            }
+        }
+    },
+    'cookie': {
+        'name': 'xtracta_login',
+        'key': 'supersecretkey123',  # Troque por uma key mais segura em produção
+        'expiry_days': 1
+    },
+    'preauthorized': {
+        'emails': []
+    }
+}
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
+name, auth_status, username = authenticator.login('Login', 'main')
+
+if auth_status is False:
+    st.error("Usuário ou senha incorretos.")
+elif auth_status is None:
+    st.warning("Por favor, insira suas credenciais.")
+elif auth_status:
+    st.sidebar.success(f"Bem-vindo, {name} 👋")
+    authenticator.logout("Logout", "sidebar")
+
+    # Aqui vai seu app    
+    logs_alerta()
+
